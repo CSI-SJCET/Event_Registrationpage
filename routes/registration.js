a6b35aeed4a4305dbdf238e672c0bcf7ffbebf59
Akhil.A <56401501+2001akhil@users.noreply.github.com>
2023-10-18 09:56:53 +0530

    error cleared

--- routes/registration.js
+++ routes/registration.js
@@ -1,45 +1,49 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const mailer=require('../node-mailer/confirmation')
-let api = express.Router();
-const registration = require("../helper/registration-helper");
+var express = require("express");
+var mongoose = require("mongoose");
+var mailer = require("../node-mailer/confirmation");
+var registration = require("../helper/registration-helper");
 
-api.post("/", async (req, res) => {
-  const newid = new mongoose.Types.ObjectId(); 
-//check the identifiers
-  const data = [
-    {
+var router = express.Router();
+
+router.post("/", function (req, res) {
+  try {
+    var newid = new mongoose.Types.ObjectId();
+    var data = {
       _id: newid,
       name: req.body.name,
       number: req.body.number,
       email: req.body.email,
       year: req.body.year,
-      brach: req.body.brach,
+      branch: req.body.branch,
       Howdidyouhearabout: req.body.social,
       queriesorSuggestion: req.body.qt,
-    },
-  ];
-console.log(data);
-  registration
-    .insert_std(data)
-    .then((result) => {
-      console.log(result.data);
-      mailer.sendEmail(req.body.name,req.body.email);
+    };
 
-      res.status(201).json({ message:result.Status, data: result.data });
-    })
-    .catch((err) => {
-      if (err.name === "MongoBulkWriteError" && err.code === 11000){
-            console.log("Duplicate_entry:",err);
-            res.status(409).json({ error: "Duplicate entry! This phonenumber or email already exists." });
-      }
-      else{
-            console.error("Error saving user:", err);
-            res.status(500).json("Bad request!");
-
-      }
-        
-    });
+    registration
+      .insert_std(data)
+      .then(function (result) {
+        console.log(result.data);
+        mailer.sendEmail(req.body.name, req.body.email);
+        res.status(201).json({ message: result.Status, data: result.data });
+      })
+      .catch(function (err) {
+        if (err.name === "MongoBulkWriteError" && err.code === 11000) {
+          console.log("Duplicate_entry:", err);
+          res
+            .status(409)
+            .json({
+              error:
+                "Duplicate entry! This phonenumber or email already exists.",
+            });
+        } else {
+          console.error("Error saving user:", err);
+          res.status(500).json("Internal Server Error");
+        }
+      });
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).json("Internal Server Error");
+  }
 });
 
-module.exports = api;
+module.exports = router;
